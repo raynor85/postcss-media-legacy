95ee8cd65be0d54d7127f7f564428d81e299169e
Ruben.Rizzi <Ruben.Rizzi@hpi.co.uk>
2015-08-26 17:28:25 +0100

    Fixed: Bug in multiple selectors inside media queries

--- README.md
+++ README.md
@@ -44,3 +44,7 @@ postcss([ require('postcss-media-legacy') ])
 ```
 
 Or take advantage of [any of the myriad other ways to consume PostCSS](https://github.com/postcss/postcss#usage), and follow the plugin instructions they provide.
+
+### Dependencies
+
+It doesn't require any dependencies, but it has to be loaded after [postcss-nested](https://github.com/postcss/postcss-nested) if included.
--- index.js
+++ index.js
@@ -10,8 +10,14 @@ module.exports = postcss.plugin('postcss-media-legacy', function mediaLegacy(opt
         css.eachAtRule(function(atRule) {
             if(atRule.name === 'media') {
                 atRule.nodes.forEach(function( node ) {
-                    var clonedNode = node.clone();
-                    clonedNode.selector = '.lt-ie9 ' + clonedNode.selector;
+                    var clonedNode = node.clone(),
+                        clonedNodeArray = clonedNode.selector.split(',');
+
+                    for (var i = 0; i < clonedNodeArray.length; i++) {
+                        clonedNodeArray[i] = '.lt-ie9 ' + clonedNodeArray[i];
+                    }
+                    clonedNode.selector = clonedNodeArray.join(',');
+                    console.log(clonedNode.selector);
                     result.root.insertBefore( atRule, clonedNode );
                 });
             }
