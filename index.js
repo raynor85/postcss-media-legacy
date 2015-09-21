140d3d50313b5cfe28b21710cb1250d81d3c0640
Ruben.Rizzi <Ruben.Rizzi@hpi.co.uk>
2015-09-21 15:19:53 +0100

    Fixed: https://github.com/raynor85/postcss-media-legacy/issues/1

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ module.exports = postcss.plugin('postcss-media-legacy', function mediaLegacy(opt
                 atRule.nodes.forEach(function( node ) {
                     var clonedNode = node.clone(),
                         clonedNodeArray = clonedNode.selector.split(','),
-                        selector = '.lt-ie9',
+                        selector = '.lt-ie9 ',
                         selectorLength = selector.length;
 
                     for (var i = 0; i < clonedNodeArray.length; i++) {
                         if(clonedNodeArray[i].substring(0, selectorLength) == selector) {
-                            clonedNodeArray[i] = substring(selectorLength);
+                            clonedNodeArray[i] = clonedNodeArray[i].substring(selectorLength);
                         }
-                        clonedNodeArray[i] = selector + ' ' + clonedNodeArray[i];
+                        clonedNodeArray[i] = selector + clonedNodeArray[i];
                     }
                     clonedNode.selector = clonedNodeArray.join(',');
                     result.root.insertBefore( atRule, clonedNode );
